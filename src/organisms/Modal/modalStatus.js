7e2c77c5fd25fd4d1b6161b363952bcdb27d141a
JagoanNeon26 <pramadiptabintang26@gmail.com>
2022-07-10 19:13:53 +0700

    fix formik radioform

--- src/organisms/Modal/modalStatus.js
+++ src/organisms/Modal/modalStatus.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { React, useState } from 'react';
 import { Modal, Alert, Button, Form, Stack } from 'react-bootstrap';
 import * as Yup from 'yup';
@@ -5,29 +6,21 @@ import { Formik, Field } from 'formik';
 import FormikController from 'components/atoms/Formik/FormikController';
 import { useDispatch } from 'react-redux';
 import styles from './baseModal.module.css';
-import RadioForm from 'components/atoms/Formik/FormikInput/FormikRadio';
 
 function FormRadio() {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
   const initialValues = {
     picked: '',
   };
 
   const validationSchema = Yup.object({
-    picked: Yup.string().required('Nama Produk diperlukan!'),
+    picked: Yup.string().required('Status Produk diperlukan!'),
   });
 
-  // const onSubmit = (values) => {
-  //   console.log('Form data', values);
-  // };
-
   const onSubmit = async (values) => {
-    await new Promise((r) => setTimeout(r, 500));
-    alert(JSON.stringify(values, null, 2));
+    console.log('Form data', values);
   };
-
-  const [show, setShow] = useState(false);
 
   return (
     <Formik
@@ -35,85 +28,87 @@ function FormRadio() {
       validationSchema={validationSchema}
       onSubmit={onSubmit}
     >
-      {({ values }) => <RadioForm />}
+      {({ values, formikProps }) => (
+        <Form>
+          <FormikController
+            control="formikradio"
+            label="Berhasil Dijual"
+            name="picked"
+            value="success"
+            formikProps={formikProps}
+          />
+          <div
+            style={{
+              color: '#8A8A8A',
+              marginLeft: '23px',
+            }}
+          >
+            Kamu telah sepakat menjual produk ini kepada pembeli
+          </div>
+          <br />
+          <FormikController
+            control="formikradio"
+            label="Batalkan Transaksi"
+            name="picked"
+            value="failed"
+            formikProps={formikProps}
+          />
+          <div
+            style={{
+              color: '#8A8A8A',
+              marginLeft: '23px',
+            }}
+          >
+            Kamu membatalkan transaksi produk ini dengan pembeli
+          </div>
+          <br />
+          {/* <div>Picked: {values.picked}</div> */}
+          <Button
+            type="submit"
+            style={{
+              width: '300px',
+              height: '42px',
+              borderRadius: '16px',
+              background: '#7126b5',
+            }}
+          >
+            Kirim
+          </Button>
+        </Form>
+      )}
     </Formik>
   );
 }
 
 export default function ModalPenawaran2(props) {
-  const [show, setShow] = useState(false);
-  const [modalShow, setModalShow] = useState(false);
-
-  if (show) {
-    return (
-      <>
-        <Alert
-          variant="success"
-          onClose={() => {
-            setShow(false);
-          }}
-          dismissible
+  return (
+    <>
+      <Modal
+        {...props}
+        dialogClassName={styles.modal}
+        contentClassName={styles.modal}
+        size="md"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header style={{ border: 'none' }} closeButton />
+        <Modal.Body>
+          <div className={styles.judulModal}>
+            Perbarui status penjualan produkmu
+          </div>
+          <div className={styles.content2}>
+            <FormRadio />
+          </div>
+        </Modal.Body>
+        <Modal.Footer
           style={{
-            zIndex: '1',
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            right: 0,
-            zIndex: 999,
+            border: 'none',
             display: 'flex',
             justifyContent: 'center',
+            paddingTop: '0px',
           }}
-        >
-          <p>Status Produk Berhasil Diperbarui</p>
-        </Alert>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <Modal
-          {...props}
-          dialogClassName={styles.modal}
-          contentClassName={styles.modal}
-          size="md"
-          aria-labelledby="contained-modal-title-vcenter"
-          centered
-        >
-          <Modal.Header style={{ border: 'none' }} closeButton />
-          <Modal.Body>
-            <div className={styles.judulModal}>
-              Perbarui status penjualan produkmu
-            </div>
-            <div className={styles.content2}>
-              <FormRadio />
-            </div>
-          </Modal.Body>
-          <Modal.Footer
-            style={{
-              border: 'none',
-              display: 'flex',
-              justifyContent: 'center',
-              paddingTop: '0px',
-            }}
-          >
-            {/* <AlertSuccess/> */}
-            <Button
-              type="submit"
-              style={{
-                width: '20vw',
-                height: '42px',
-                borderRadius: '16px',
-                background: '#7126b5',
-              }}
-              onClick={() => {
-                setShow(true);
-              }}
-            >
-              Kirim
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </>
-    );
-  }
+        ></Modal.Footer>
+      </Modal>
+    </>
+  );
 }
